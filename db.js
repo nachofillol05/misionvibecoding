505bbc15c2440271cb2f7541af0782421a319d26
Ignacio Fillol <94014921+nachofillol05@users.noreply.github.com>
2025-12-20 16:06:59 -0300

    cambio en la conexion a la bd para limitarlas

--- db.js
+++ db.js
@@ -2,9 +2,11 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false },
+  max: 3,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
 });
+
 
 module.exports = pool;
